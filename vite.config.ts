238a474a507315b26ccd4bc199fb408693d2b1d3
sahilkasar01 <sahil.1251070434@vit.edu>
2026-09-11 01:27:34 +0530

    add changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['two6001-2.onrender.com']
+    allowedHosts: true
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: true
   }
 })
