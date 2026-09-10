e7d22537fecf2c11a36445b2731d41c93971c53f
sahilkasar01 <sahil.1251070434@vit.edu>
2026-09-11 01:40:59 +0530

    add changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: '0.0.0.0',
-    allowedHosts: true
+    allowedHosts: true,
   },
-
   preview: {
     host: '0.0.0.0',
     port: 10000,
-    allowedHosts: true
-  }
-})
+    allowedHosts: true,
+  },
+});
